a9b4f0491d64cd9e5a149cb005070b19de516a0d
Lee Hughes <nomoreweirdmachines@gmail.com>
2019-10-07 11:37:35 +0100

    first

---
+++ nixlang-nix/install-nix.sh
@@ -0,0 +1,14 @@
+#create a normal user
+#install nix package manager
+
+#create a nornal user who can sudo
+
+useradd -m nixuser -G sudo;su - nixuser 
+
+#Install the nix package manager into your enviroment
+
+curl https://nixos.org/nix/install | sh
+
+. ./home/nixuser/.nix-profile/etc/profile.d/nix.sh`
+echo ". ./home/nixuser/.nix-profile/etc/profile.d/nix.sh"
+
